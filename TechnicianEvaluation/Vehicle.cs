cab8fce8c9229f1dee615b5e77026c0323d24110
slufkins <69993706+slufkins@users.noreply.github.com>
2023-12-11 14:02:58 -0600

    Added vehicle class

---
+++ TechnicianEvaluation/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicianEvaluation
+{
+    internal class Vehicle
+    {
+        readonly int _year;
+        readonly string _make;
+        readonly string _model;
+        public Vehicle(int year, string make, string model)
+        {
+            _year = year;
+            _make = make;
+            _model = model;
+        }
+
+        public int Year
+        {
+            get { return _year; }
+        }
+
+        public string Make 
+        { 
+            get { return _make;} 
+        }
+
+        public string Model
+        {
+            get { return _model;}
+        }
+    }
+}
