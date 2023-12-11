edbd5fb89cfd48b34437306c8e2cbe21b71716d0
slufkins <69993706+slufkins@users.noreply.github.com>
2023-12-11 13:53:05 -0600

    Added Properties to Technician class

--- TechnicianEvaluation/Technician.cs
+++ TechnicianEvaluation/Technician.cs
@@ -8,10 +8,10 @@ namespace TechnicianEvaluation
 {
     internal class Technician
     {
-        int _techID;
+        readonly int _techID;
         string _fname;
         string _lname;
-        char _skill;
+        double _skill;
         double _efficiency;
 
         // New technician constructor
@@ -26,13 +26,65 @@ namespace TechnicianEvaluation
         }
 
         // Experienced technician constructor
-        public Technician(int id, string fname, string lname, char skill, double efficiency)
+        public Technician(int id, string fname, string lname, double skill, double efficiency)
         {
             _techID = id;
             _fname = fname;
             _lname = lname;
             _skill = skill;
             _efficiency = efficiency;
+
+            // Add current skill to skill list and current efficiency to efficiency list
+            skillList.Add(skill);
+            efficiencyList.Add(efficiency);
+        }
+
+        List<double> skillList = new List<double>();
+        List<double> efficiencyList = new List<double>();
+
+        public int TechID
+        {
+            get { return _techID; }
+        }
+
+        public string FirstName
+        {
+            get { return _fname; }
+            set { _fname = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lname; }
+            set { _lname = value; }
+        }
+
+        public double Skill
+        {
+            get { return _skill; }
+            set
+            {
+                skillList.Add(value);
+
+                double total = skillList.Sum();
+                double numOfEntries = skillList.Count();
+
+                _skill = total / numOfEntries;
+            }
+        }
+
+        public double Efficiency
+        { 
+            get { return _efficiency; }
+            set 
+            {
+                efficiencyList.Add(value);
+
+                double total = efficiencyList.Sum();
+                double numOfEntries = efficiencyList.Count();
+
+                _efficiency = total / numOfEntries;
+            } 
         }
     }
 }
